80c0791bf564e6566f907327484a9d4b30612d27
Markazmi <Mariakazmi81@gmail.com>
2021-12-29 08:26:15 +0500

    Error Handling

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,22 @@
 const dot =require('dotenv')
 const app=require("./app.js");
 const connectDatabase = require('./config/db.js');
+const error = require('./middlewares/error.js');
 // path for dotenv file
+
+// process.on("Uncaught exception", error =>{
+//     console.log(`Error: ${error.stack}`)
+//     console.log("Shutting down the server due to UnCaught Exceptions");
+//     process.exit(1)
+// })
 dot.config({path: 'backend/config/config.env'});
 const port = process.env.PORT || 5000;
 connectDatabase();
 app.listen(port, ()=>{
     console.log(`server is listening ON ${port} in ${process.env.NODE_ENV} mode`);
 })
+// process.on("UnhandledRejection", error =>{
+//     console.log(`Error:${error.stack}`)
+//     console.log('Shutting down the server due to Unhandle promise Exceptions');
+//     process.exit(1)
+// })
