92a8512fe5110bff70a411b5647018e6b83ea7e0
Markazmi <Mariakazmi81@gmail.com>
2022-01-22 00:10:11 +0500

    initial commit

--- backend/controllers/ProductControllers.js
+++ backend/controllers/ProductControllers.js
@@ -4,7 +4,7 @@ const ErrorHandler = require('../Utils/ErrorHandler.js')
 const APIFeatures = require('../utils/apiFeatures');
 // create new product /app/v1/product/new
 exports.newProduct = CatchAsyncerror( async(req,res,next)=>{
-  // req.body.user=req.user.id;
+  req.body.user=req.user.id;
    const product= await Products.create(req.body)
  
 res.json({
@@ -71,4 +71,88 @@ await product.remove();
 res.json({success:true,
 message:"product deleted"
 });
+})
+// create and update uproduct review
+exports.CreateProductReview = CatchAsyncerror(async(req,res,next)=>{
+  const {rating, comment, productId} = req.body
+  const newReview ={
+    name: req.user.name,
+    // we will give ratinds in strong in potman so convert in number
+    rating: Number(rating),
+    comment,
+    user: req.user._id,
+  };
+// update existing review
+const product = await Products.findById(productId);
+// console.log(product);
+const isReviewed = product.reveiws.find(
+  (r) => r.user.toString() === req.user._id.toString()
+)
+
+if(isReviewed){
+  product.reveiws.forEach((review)=>{
+  if(review.user.toString() === req.user._id.toString())
+  {
+  review.comment = comment
+  review.rating = rating
+  }
+})
+}
+
+else{
+  product.reveiws.push(newReview)
+  product.Numofreveiws=product.reveiws.length;
+}
+product.rating =
+product.reveiws.reduce((acc,item)=> item.rating + acc ,0)/
+product.reveiws.length;
+await product.save({validateBeforeSave:false})
+
+res.json({success:true})
+})
+
+
+// get all reviews
+exports.getAllReviews = CatchAsyncerror(async(req,res,next)=>{
+
+  const product = await Products.findById(req.query.id)
+  // check db
+  const reviews = product.reveiws
+  const numofreviews=product.Numofreveiws
+
+res.json({
+  numofreviews,
+  success:true,
+  message:"cwc",
+  reviews,
+  
+})
+})
+exports.deleteReview = CatchAsyncerror(async(req,res,next)=>{
+  const product = await Products.findById(req.query.productId)
+
+  const reviews = product.reveiws.filter(
+    // check all review id's !== the one you wantot delete
+    (review)=>review._id.toString() !== req.query.id.toString()
+    )
+    // why do we need review_id for
+    // http://localhost:5500/api/v1/reviews?productId=61cdc73376343239506385a3&&id=61e87d3abdb0f934292644d4
+
+  // cupdating some fields
+
+  const Numofreveiws=reviews.length
+const rating =
+product.reveiws.reduce((acc,item)=> item.rating+acc,0) /
+reviews.length
+
+// { what i want to update}
+await Products.findByIdAndUpdate(req.query.productId, {reviews,Numofreveiws, rating},{
+  new:true,
+  runValidators:true,
+  useFindAndModify:false
+})
+
+res.json({
+  sucess:true
+})
 })
